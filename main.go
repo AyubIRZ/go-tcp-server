ba9a5451d5cf6758cc553990c91051a5a8dfc465
AyubIRZ <ayubirazeh@gmail.com>
2020-04-24 18:36:02 +0430

    create go.mod

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net"
+	"sync"
 )
 
+const TCPAddr = "localhost:6060"
+
 func main() {
-	fmt.Println("it works!!!")
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	fmt.Println("TCP server started . . .")
+
+	wg.Add(1)
+	go handleListener(wg)
+}
+
+func handleListener(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	listener, err := net.Listen("tcp", TCPAddr)
+	if err != nil {
+		log.Fatal("TCP listener error: ", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("TCP conn failed: ", err)
+			continue
+		}
+
+		fmt.Println("New conn accepted! ", conn.RemoteAddr())
+
+		wg.Add(1)
+		go handleConn(conn, wg)
+	}
+}
+
+func handleConn(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer conn.Close()
+
+	msg, _ := bufio.NewReader(conn).ReadString('\n')
+	//conn.Write([]byte("heeeeyyyyyyyyyyyy!!!!!"))
+
+	fmt.Println(msg)
 }
