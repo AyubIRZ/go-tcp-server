ae29f7118c5f599e3fbe24d044bb6ecdabfae11f
AyubIRZ <ayubirazeh@gmail.com>
2020-04-29 12:25:42 +0430

    refactor error checking in GetConn

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -16,38 +16,38 @@ type Sessions struct {
 
 
 // AddConn adds a new connection to the sessions map.
-func (b *Sessions) AddConn(uniqueID ConnID, conn net.Conn) error {
-	b.locker.RLock()
-	_, ok := b.conns[uniqueID]
-	b.locker.RUnlock()
+func (s *Sessions) AddConn(uniqueID ConnID, conn net.Conn) error {
+	s.locker.RLock()
+	_, ok := s.conns[uniqueID]
+	s.locker.RUnlock()
 	if ok {
 		return errors.New("ID already exists in the connections.")
 	}
 
-	b.locker.Lock()
-	b.conns[uniqueID] = conn
-	b.locker.Unlock()
+	s.locker.Lock()
+	s.conns[uniqueID] = conn
+	s.locker.Unlock()
 
 	return nil
 }
 
 
 // GetConn returns a TCP connection based on a given ID, or error if the given ID doesn't exist in the map.
-func (b *Sessions) GetConn(uniqueID ConnID) (net.Conn, error) {
-	b.locker.RLock()
-	v, ok := b.conns[uniqueID]
-	b.locker.RUnlock()
-	if ok && v != nil {
-		return v, nil
+func (s *Sessions) GetConn(uniqueID ConnID) (net.Conn, error) {
+	s.locker.RLock()
+	v, ok := s.conns[uniqueID]
+	s.locker.RUnlock()
+	if !ok {
+		return nil, errors.New("Requested TCP connection doesn't exist.")
 	}
 
-	return nil, errors.New("Requested TCP connection doesn't exist.")
+	return v, nil
 }
 
 
 // DeleteConn removes a TCP connection based on a given ID.
-func (b *Sessions) DeleteConn(uniqueID ConnID) {
-	b.locker.Lock()
-	delete(b.conns, uniqueID)
-	b.locker.Unlock()
+func (s *Sessions) DeleteConn(uniqueID ConnID) {
+	s.locker.Lock()
+	delete(s.conns, uniqueID)
+	s.locker.Unlock()
 }
